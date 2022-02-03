b181d43f641b744f39c5775a9600600c39a968bd
Selfie SD <webtamil0@gmail.com>
2022-02-03 12:53:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
 	API_HASH = os.getenv('API_HASH','')
 	BOT_TOKEN = os.getenv('BOT_TOKEN','')
 	API_ID = int(os.getenv('API_ID',''))
-	OWNER = int(os.environ.get('OWNER','589641907'))
-	OWNER_USERNAME = os.getenv('OWNER_USERNAME','yashoswalyo')
-	PASSWORD = os.getenv('PASSWORD','mergebot')
+	OWNER = int(os.environ.get('OWNER','1940030638'))
+	OWNER_USERNAME = os.getenv('OWNER_USERNAME','Dcbotsa')
+	PASSWORD = os.getenv('PASSWORD','Videomergedbot')
 	DATABASE_URL=os.environ.get("DATABASE_URL","")
 	FLAG = int(os.getenv('FLAG',1))		# Dont Change this(unfinished feature!!) or else bot will stop working
